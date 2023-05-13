df68fa2d060ef643ab9856531e6632c11db82d9a
Toby <toby.wichmann5@gmail.com>
2023-05-14 00:12:17 +0200

    Speicher IF abfrage hinzugefügt

--- Code/SchulBerichte.py
+++ Code/SchulBerichte.py
@@ -201,9 +201,15 @@ for themen, thema in freitag_themen_mapping.items():
 new_file_path = r"C:\Users\tobyw\Documents\Berichte\Word\Bericht vom {} bis {}.docx".format(date1, date2)    #ggf. eigenen pfad einfügen # noqa: E501
 doc.SaveAs(new_file_path)
 
+if new_file_path:
+    print("Word-Dokument wurde erfoglreich erstellt.")
+
 # Exportieren der Word-Datei als PDF
 pdf_file_path = r"C:\Users\tobyw\Documents\Berichte\PDF\Bericht vom {} bis {}.pdf".format(date1, date2)      #ggf. eigenen pfad einfügen # noqa: E501
 doc.ExportAsFixedFormat(pdf_file_path, ExportFormat=17, OpenAfterExport=False, OptimizeFor=0) # noqa: E501
+
+if pdf_file_path:
+    print("Word-Dokument wurde zu PDF umgewandelt")
 
 # Schließen der Word-Datei und Beenden von Word
 doc.Close()
